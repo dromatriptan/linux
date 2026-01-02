1efaf42434a924ad44ab1bb45c64e0993341fbb5
dromatriptan <dromatriptan@outlook.com>
2026-01-02 08:48:29 -0500

    Included code snippet from stack overflow's tlrobinson to resolve the script's residing path when called from a symbolic link.

--- Nextcloud/Update-Talk.sh
+++ Nextcloud/Update-Talk.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+# Source - https://stackoverflow.com/a
+# Posted by tlrobinson, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-31, License - CC BY-SA 3.0
+
+# get the absolute path of the executable
+SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH=$SELF_PATH/$(basename -- "$0")
+SCRIPT_PATH=$(cd "$DIR" && cd "$(dirname -- "$SYM")" && pwd)
+
+# resolve symlinks
+while [[ -h $SELF_PATH ]]; do
+    # 1) cd to directory of the symlink
+    # 2) cd to the directory of where the symlink points
+    # 3) get the pwd
+    # 4) append the basename
+    DIR=$(dirname -- "$SELF_PATH")
+    SYM=$(readlink "$SELF_PATH")
+    SELF_PATH=$(cd "$DIR" && cd "$(dirname -- "$SYM")" && pwd)/$(basename -- "$SYM")
+	SCRIPT_PATH=$(cd "$DIR" && cd "$(dirname -- "$SYM")" && pwd)
+done
+
 export pwsh_version=$(pwsh --version)
 
 if [[ -z "$pwsh_version" ]]; then
 	echo "Missing Powershell, installing"
-	sudo ./Install-Pwsh.sh
+	sudo "$SCRIPT_PATH/Install-Pwsh.sh"
 else
-	pwsh -file ./Update-Nextcloud.ps1 -appName talk -Install
+	pwsh -file "$SCRIPT_PATH/Update-Nextcloud.ps1" -appName talk -Install
 fi
