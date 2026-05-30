a181db5a04bcd648963c58daae0031860784801e
dromatriptan <dromatriptan@outlook.com>
2026-05-30 16:12:35 -0400

    Add files via upload
    I tweaked a few things to make the code more resilient.

--- Nextcloud/Install-Pwsh.sh
+++ Nextcloud/Install-Pwsh.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-export mint_version="24.04"
 apt-get update
 apt-get install -y wget
-wget -q https://packages.microsoft.com/config/ubuntu/$mint_version/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 apt-get update
